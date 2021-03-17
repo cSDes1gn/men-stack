6c98183f6ee6e3bbf0ef74c256d75696137d46fb
cSDes1gn <christian911@sympatico.ca>
2021-03-17 02:09:16 -0400

    tabbing

--- routes/index.js
+++ routes/index.js
@@ -8,50 +8,50 @@ router.get('/', function(req, res, next) {
 
 /* GET Hello World Page*/
 router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello World' });
+  res.render('helloworld', { title: 'Hello World' });
 });
 
 /* GET Userlist page. */
 router.get('/userlist', function(req,res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({}, {}, function(e,docs) {
-        res.render('userlist', {
-            "userlist": docs
-        });
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e,docs) {
+    res.render('userlist', {
+      "userlist": docs
     });
+  });
 });
 
 /* GET New User page. */
 router.get('/newuser', function(req,res) {
-    res.render('newuser', { title: "Add New User" });
+  res.render('newuser', { title: "Add New User" });
 });
 
 /* POST to Add User Service */
 router.post('/adduser', function(req,res){
-    // set our internal db var
-    var db = req.db;
-    
-    // get our form values (using name attr)
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
+  // set our internal db var
+  var db = req.db;
+  
+  // get our form values (using name attr)
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
 
-    // set collection
-    var collection = db.get('usercollection');
+  // set collection
+  var collection = db.get('usercollection');
 
-    // submit to db
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function(err, doc) {
-        if (err) {
-            // return error response
-            res.send("There was a problem adding the requested entries to the database.");
-        } else {
-            // forward success back to userlist
-            res.redirect("userlist")
-        }
-    });
+  // submit to db
+  collection.insert({
+    "username" : userName,
+    "email" : userEmail
+  }, function(err, doc) {
+    if (err) {
+      // return error response
+      res.send("There was a problem adding the requested entries to the database.");
+    } else {
+      // forward success back to userlist
+      res.redirect("userlist")
+    }
+  });
 });
 
 
