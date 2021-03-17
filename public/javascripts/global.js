cc81f69199a3c9fd46731c1008e42286b1631008
cSDes1gn <christian911@sympatico.ca>
2021-03-17 02:42:39 -0400

    populate user info panel

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -4,6 +4,8 @@ var userListData = [];
 $(document).ready(function(){
   // populate table on page load
   populateTable();
+  // Username link click
+  $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
 // fill table with data
@@ -11,6 +13,8 @@ function populateTable() {
   var tableContent = '';
   //jQuery AJAX call for json
   $.getJSON('/users/userlist', function(data){
+    // take a snapshot of the userlist data into global var for universal access
+    userListData = data;
     // for each utem in the json document add a table row and cells to the content
     $.each(data, function(){
       tableContent += '<tr>';
@@ -22,4 +26,20 @@ function populateTable() {
     // inject the content string into our html table
     $('#userList table tbody').html(tableContent);
   });
+};
+
+function showUserInfo(event){
+  // prevent link from firing
+  event.preventDefault();
+  // get username from link rel attribute
+  var thisUserName = $(this).attr('rel');
+  // get index of object from id value
+  var arrayPosition = userListData.map(function(arrayItem){ return arrayItem.username; }).indexOf(thisUserName);
+  // get user object
+  var thisUserObject = userListData[arrayPosition];
+  // populate info box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
 };
